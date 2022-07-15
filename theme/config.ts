0d403afe2a342cafe6ceff1b2b871d08470da218
brightiron <brightiron@protonmail.com>
2022-07-14 19:00:05 -0500

    updating font

--- theme/config.ts
+++ theme/config.ts
@@ -1,6 +1,10 @@
 import { createTheme } from "@mui/material/styles";
+import NHaasGroteskRegular from "../fonts/NHaasGroteskDSPro-65Md.woff2";
 
 export const theme = createTheme({
+  typography: {
+    fontFamily: "NHassGrotesk, Helvetica, Arial, sans-serif",
+  },
   palette: {
     mode: "dark",
     primary: {
@@ -23,6 +27,18 @@ export const theme = createTheme({
     },
   },
   components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+      @font-face {
+        font-family: "NHassGrotesk";
+        font-style: normal;
+        font-display: swap;
+        font-weight: 400;
+        src: local('NHassGrotesk'), local('NHassGrotesk-55Rg'), url(${NHaasGroteskRegular}) format('woff');
+        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+      }
+      `,
+    },
     MuiButton: {
       styleOverrides: {
         containedPrimary: {
